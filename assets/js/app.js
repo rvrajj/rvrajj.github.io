974f9f3896c769b030bdcfa8f824d3888179b948
Ravindra Singh Rajpurohit <58661160+rvrajj@users.noreply.github.com>
2024-11-28 23:17:13 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,51 +1,28 @@
 // Dom7
 var $ = Dom7;
 
-// Demo
-if (document.location.href.includes('safe-areas')) {
-  const html = document.documentElement;
-  if (html) {
-    html.style.setProperty('--f7-safe-area-top', '44px');
-    html.style.setProperty('--f7-safe-area-bottom', '34px');
-  }
-}
-if (document.location.href.includes('example-preview')) {
-  $('.view-main').attr('data-browser-history', 'true');
-  $('.view-main').attr('data-browser-history-root', '/');
-  $('.view-main').attr('data-preload-previous-page', 'false');
-  $('.view-main').attr('data-ios-swipe-back', 'false');
-  document.documentElement.classList.add('example-preview');
-}
-
 // Theme
-var theme = 'md';
+var theme = 'ios';
 document.documentElement.classList.add('dark');
 // Init App
 var app = new Framework7({
   el: '#app',
   theme,
-  view: {
-    componentCache: false,
-    browserHistory: false,
+  darkMode: localStorage?.getItem('darkmode-'+theme) == "false" ? false: true,
+  colors: {
+    primary: '#009688',
+    azure: '#007aff'
   },
-  // store.js,
+  iosTouchRipple: true,
+  //pushState: true,
   store: store,
-  darkMode: localStorage?.getItem('darkmode-'+theme) == "false" ? false : true,
-  // routes.js,
   routes: routes,
-  popup: {
-    closeOnEscape: true,
-  },
-  sheet: {
-    closeOnEscape: true,
-  },
-  popover: {
-    closeOnEscape: true,
-  },
-  actions: {
-    closeOnEscape: true,
-  },
   vi: {
     //placementId: 'pltd4o7ibb9rc6i53x14',
   },
+  on: {
+    pageInit: function () {
+      console.log('Page initialized');
+    },
+  }
 });
